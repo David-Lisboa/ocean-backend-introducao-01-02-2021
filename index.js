6bc29e6b664c8b92059c6e8ec25d120558d11c87
David-Lisboa <dlrlisboa@gmail.com>
2021-02-02 18:47:51 -0400

    Criação da const mensagem

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express()
 
 const port = 3000;
 
+const mensagens = [
+  "primeira mensagem",
+  "Segunda mensagem"
+];
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
   })
 
 app.get('/mensagens', (req, res) => {
-    res.send('Exibir todas as mensagens.')
-    console.info('Servidor rodando em http://localhost:'+ port)
-  })
+    res.send(mensagens)
+  });
 
-app.listen(3000)
+app.listen(port, ()=>{
+  console.info('Servidor rodando em http://localhost:'+ port);
+});
