31c22a650ab2cdb72132c09db084b401c4b3d3eb
David-Lisboa <dlrlisboa@gmail.com>
2021-02-02 19:18:37 -0400

    Atualização apartir do arquivo do orientador

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+onst express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 
 const port = 3000;
 
+/*
+Create, Read (All/Single), Update & Delete
+Criar, Ler (Tudo ou Individual), Atualizar e Remover
+*/
+
 const mensagens = [
-  "primeira mensagem",
+  "Primeira mensagem",
   "Segunda mensagem"
 ];
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-  })
+  res.send('Hello World!');
+});
 
+// Ler Tudo (Read All)
 app.get('/mensagens', (req, res) => {
-    res.send(mensagens)
-  });
+  res.send(mensagens);
+});
 
-app.post('/mensagens,', (req, res) =>{
-  const mensagem = req.body.texto;
-  mensagem.push(mensagens);
-
-  res.send('Mensagem criada com sucesso.')
+app.get('/mensagens/:id,', (req, res) =>{
+   console.log(req.params)
+   res.send('Ler individual')
 } );
+  
+// Criar (Create)
+app.post('/mensagens', (req, res) => {
+  const mensagem = req.body.texto;
 
-app.listen(port, ()=>{
-  console.info('Servidor rodando em http://localhost:'+ port);
+  mensagens.push(mensagem);
+
+  res.send('Mensagem criada com sucesso.');
+});
+
+app.listen(port, () => {
+  console.info('Servidor rodando em http://localhost:' + port);
 });
